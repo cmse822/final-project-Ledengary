5a6090a63f11774e959fcee2ebd378c46cbb174a
Reza <reza@gamertag.app>
2024-04-16 11:15:00 -0400

    initialized the Cluster class

---
+++ Cluster.h
@@ -0,0 +1,10 @@
+#include "Dot.h"
+
+class Cluster{
+    private:
+        double x_coordinate;
+        double y_coordinate;
+        int number_of_points; 
+        double x_coordinate_total;
+        double y_coordinate_total;
+};

14dd272414a3ca060386d545a84365e89c32d711
Reza <reza@gamertag.app>
2024-04-16 10:58:08 -0400

    added the Dot class

---
+++ Point.h
@@ -0,0 +1,10 @@
+#ifndef PC_DOT_H
+#define PC_DOT_H
+
+class Dot{
+
+private:
+    double x_coordinate; 
+    double y_coordinate;
+    int cluster_id; 
+}
---
+++ sequential_main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <chrono>
+#include <cmath>
+#include <omp.h>
+#include "Dot.h"
+#include "Cluster.h"
+#include "fstream"
+using namespace std;
+

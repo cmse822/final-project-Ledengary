6f35d690ebf4072a846adf0c65e05d06aa14c9d1
Reza <reza@gamertag.app>
2024-04-17 14:13:23 -0400

    addded initial omp functions to  the Cluster class

--- sequential_main.cpp
+++ sequential_main.cpp
@@ -3,7 +3,13 @@
 #include <cmath>
 #include <omp.h>
 #include "Dot.h"
-// #include "Cluster.h"
+#include "Cluster.h"
 #include "fstream"
 using namespace std;
 
+
+int number_of_points = 500000;
+int  number_of_clusters = 1000;
+
+vector<Dot> create_point(int number_of_points);
+vector<Cluster> create_cluster(int number_of_clusters);

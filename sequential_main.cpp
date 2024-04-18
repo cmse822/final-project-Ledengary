f86e308e3269c5043512e8d4b18065753b486b1b
Reza <reza@gamertag.app>
2024-04-18 11:00:21 -0400

    included the initial main in sequential main

--- sequential_main.cpp
+++ sequential_main.cpp
@@ -13,3 +13,35 @@ int  number_of_clusters = 10;
 
 vector<Dot> create_point(int number_of_points);
 vector<Cluster> create_cluster(int number_of_clusters);
+
+
+int main() {
+
+    srand(time(NULL));
+    printf("Number of Points %d\n",number_of_points);
+    printf("Number of Clusters %d\n",number_of_clusters);
+
+    double start_time = omp_get_wtime();
+    printf("Initialization \n");
+
+    printf("Creation of the Points \n");
+    vector<Dot> pts = create_point(number_of_points);
+    printf("Points Created \n");
+
+    printf("Creations of the Clusters \n");
+    vector<Cluster> cls = create_cluster(number_of_clusters);
+    printf("Clusters Created \n");
+
+    double end_time1 = omp_get_wtime();
+
+    auto duration = end_time1 - start_time;
+    printf("Initialization made in: %f seconds\n",duration);
+
+    int iteration_num = 0;
+    bool iterate = true;
+    printf("-STARTING ITERATE-\n");
+
+
+
+    return 0;
+}

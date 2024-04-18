68562f60c88421c51b38ce425e561d9e237b8e1a
Reza <reza@gamertag.app>
2024-04-18 11:05:36 -0400

    refined main in sequential main

--- sequential_main.cpp
+++ sequential_main.cpp
@@ -8,32 +8,31 @@
 using namespace std;
 
 
-int number_of_points = 500;
+int number_of_dots = 500;
 int  number_of_clusters = 10;
+int iterations = 20;
 
-vector<Dot> create_point(int number_of_points);
+vector<Dot> create_point(int number_of_dots);
 vector<Cluster> create_cluster(int number_of_clusters);
-
+void plot(vector<Dot> &dots);
 
 int main() {
-
     srand(time(NULL));
-    printf("Number of Points %d\n",number_of_points);
+    printf("Number of dots %d\n",number_of_dots);
     printf("Number of Clusters %d\n",number_of_clusters);
 
     double start_time = omp_get_wtime();
     printf("Initialization \n");
 
-    printf("Creation of the Points \n");
-    vector<Dot> pts = create_point(number_of_points);
-    printf("Points Created \n");
+    printf("Creation of the dots \n");
+    vector<Dot> pts = create_point(number_of_dots);
+    printf("dots Created \n");
 
     printf("Creations of the Clusters \n");
     vector<Cluster> cls = create_cluster(number_of_clusters);
     printf("Clusters Created \n");
 
     double end_time1 = omp_get_wtime();
-
     auto duration = end_time1 - start_time;
     printf("Initialization made in: %f seconds\n",duration);
 
@@ -41,7 +40,23 @@ int main() {
     bool iterate = true;
     printf("-STARTING ITERATE-\n");
 
-
-
+    while(iteration_num < iterations && iterate){
+        iteration_num ++;
+    }
+    double end_time2 = omp_get_wtime();
+    duration = end_time2 - end_time1;
+    printf("Number of iterarions %d, total time %f seconds, iteration time avg %f seconds \n", iteration_num,duration, duration/iteration_num);
+    printf("Storing the dots coordinates and cluster-id...\n");
+    plot(pts);
     return 0;
+}
+
+void plot(vector<Dot> &dots){
+    ofstream Myfile("data.txt");
+    Myfile << "x,y,cluster_id"<< endl ;
+    for(int i = 0; i < dots.size(); i++){
+        Dot point = dots[i];
+        Myfile << point.get_x() << "," << point.get_y() << "," << point.get_cluster_id() << endl;
+    }
+    Myfile.close();
 }

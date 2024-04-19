0c7b886c34b48831f5edf88a3f9c733ac4ebf0d3
Reza <reza@gamertag.app>
2024-04-19 11:22:30 -0400

    added the initial headers for parallel knn

--- Parallel-KNN.cpp
+++ Parallel-KNN.cpp
@@ -3,6 +3,20 @@
 #include <cmath>
 #include <omp.h>
 #include <fstream>
-#include "Point.h"
+#include "Dot.h"
 #include "Cluster.h"
 using namespace std;
+
+int number_of_dots = 500;
+int  number_of_clusters = 10;
+int iterations = 20;
+double max_value = 100000;
+int number_of_threads = 10;
+
+vector<Dot> create_point(int num_pt);
+vector<Cluster> create_cluster(int num_cl);
+void find_distance(vector<Dot> &pts, vector<Cluster> &cls);
+double euclidean_dist(Dot pt, Cluster cl);
+bool update_clusters(vector<Cluster> &cls);
+void plot(vector<Dot> &points);
+
